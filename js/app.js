7b67f125772d88e36883940e2c2ddff7dee51236
jessikim <jessica.kim17@gmail.com>
2022-08-30 16:34:57 -0500

    Complete 5-6

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const inputElement = document.getElementById("input")
+const btnElement = document.getElementById("submit-button")
+const listElement = document.getElementById("todo-list")
+
+btnElement.addEventListener('click', function(evt){
+  // create a new li element
+  const li = document.createElement('li')
+  // console.log(li);
+  
+  // take the user's input value and set it equal to the new li element
+  li.textContent = input.value
+  // console.log(li.textContent);
+  
+  // reset the text in input field to an empty string
+  input.value = ''
+
+  // add a new li element to the page INSIDE the ul element
+  document.querySelector('ul').appendChild(li)
+})
