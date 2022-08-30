b6a6745af450a021a9494273e900343f949d9736
jessikim <jessica.kim17@gmail.com>
2022-08-30 17:08:27 -0500

    Add Reset Button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const inputElement = document.getElementById("input")
 const btnElement = document.getElementById("submit-button")
 const listElement = document.getElementById("todo-list")
+const resetBtn = document.getElementById("reset")
 
 btnElement.addEventListener('click', function(evt){
   // create a new li element
@@ -16,4 +17,8 @@ btnElement.addEventListener('click', function(evt){
 
   // add a new li element to the page INSIDE the ul element
   document.querySelector('ul').appendChild(li)
+})
+
+resetBtn.addEventListener('click', function(evt){
+  listElement.remove()
 })
