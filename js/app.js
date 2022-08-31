fecbfb7d2c956acb504ba5e2b8a96b4787281087
jessikim <jessica.kim17@gmail.com>
2022-08-31 16:54:37 -0500

    Add features to an empty item

--- js/app.js
+++ js/app.js
@@ -9,16 +9,29 @@ btnElement.addEventListener('click', function(evt){
   // create a new li element
   const li = document.createElement('li')
   // console.log(li);
+
+  // don't allow empty items to be added to the to-do list
+  if (input.value === '') {
+    inputElement.style.backgroundColor = "lightgray";
+    input.value = "type something!"
+
+    inputElement.addEventListener('click', function(evt) {
+      input.value = ""
+      inputElement.style.backgroundColor = "white";
+    })
+
+  } else {
+    // take the user's input value and set it equal to the new li element
+    li.textContent = input.value
+    // console.log(li.textContent);
+    
+    // reset the text in input field to an empty string
+    input.value = ''
+    
+    // add a new li element to the page INSIDE the ul element
+    document.querySelector('ul').appendChild(li)
+  }
   
-  // take the user's input value and set it equal to the new li element
-  li.textContent = input.value
-  // console.log(li.textContent);
-  
-  // reset the text in input field to an empty string
-  input.value = ''
-  
-  // add a new li element to the page INSIDE the ul element
-  document.querySelector('ul').appendChild(li)
 })
 
 
